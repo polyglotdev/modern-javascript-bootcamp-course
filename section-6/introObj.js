655da87e6fe8752db0f485c2f0282de5f6b60a03
Dom Hallan <dom@polyglotdev.com>
2021-06-29 16:26:25 -0500

    🎆 start section 6

---
+++ section-6/introObj.js
@@ -0,0 +1,23 @@
+const fitBitData = {
+  totalSteps: 30000,
+}
+
+console.log(fitBitData.totalSteps)
+fitBitData.totalSteps += 30
+console.log(fitBitData.totalSteps)
+
+const obj = {
+  name: 'Elijah Hallan',
+  age: 5,
+  gradeLevel: 'Kindergarten',
+}
+
+console.log(obj.name)
+
+const numbers = {
+  100: 100,
+  200: 200,
+  300: 300,
+}
+
+console.log(numbers[100])
