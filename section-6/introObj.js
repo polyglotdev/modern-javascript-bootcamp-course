43533eca4c53db9e5937ce66082aef99d7c99a51
Dom Hallan <dom@polyglotdev.com>
2021-06-30 13:36:06 -0500

    🎆 end section 6

--- section-6/introObj.js
+++ section-6/introObj.js
@@ -21,3 +21,48 @@ const numbers = {
 }
 
 console.log(numbers[100])
+
+const palette = {
+  red: '#eb4d4b',
+  yellow: '#f9ca24',
+  blue: '#30336b',
+  '76Trombones': 'song',
+}
+
+console.log(palette['red'])
+const userReviews = {}
+
+const shoppingCart = [
+  {
+    product: 'Apple iPad 13 inch',
+    price: 699.0,
+    quantity: 1,
+  },
+  {
+    product: 'Apple iPhone 12 Pro Max',
+    price: 1300.0,
+    quantity: 1,
+  },
+]
+
+const student = {
+  firstName: 'Elijah',
+  lastName: 'Hallan',
+  exams: {
+    midTerm: 92,
+    final: 88,
+  },
+}
+
+const average = (student.exams.midTerm + student.exams.final) / 2
+console.log(average)
+
+const getCartTotal = () => {
+  let total
+  shoppingCart.forEach((item) => {
+    total = item.price += item.price
+  })
+  return total
+}
+
+getCartTotal() /* ? */
