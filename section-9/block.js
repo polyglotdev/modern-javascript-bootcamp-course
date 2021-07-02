4f820aae4271e80f585d4876e6507a30c9084bcc
Dom Hallan <dom@polyglotdev.com>
2021-07-01 20:18:31 -0500

    :sparkles: add 2 i variables in the same block scope

---
+++ section-9/block.js
@@ -0,0 +1,15 @@
+const printAnimal = (animal) => {
+  if (true) {
+    let animal = `eel`
+    console.log(animal)
+  }
+}
+
+printAnimal(`dog`)
+
+let animals = [`dog`, `cat`, `fish`, `bird`]
+let i = 10
+for (let i = 0; i < animals.length; i++) {
+  console.log(i, animals[i])
+}
+console.log(i)
