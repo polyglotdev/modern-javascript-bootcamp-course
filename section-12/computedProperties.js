70edd970634dd52ae58dc85281075a69866c7a65
Dom Hallan <dom@polyglotdev.com>
2021-07-10 23:24:45 -0500

    🐍 complete computed properties

--- section-12/computedProperties.js
+++ section-12/computedProperties.js
@@ -5,3 +5,21 @@ const team = {
   [role]: person,
 }
 console.log(team)
+
+// create a function that accepts and object and then copies the object with a new property added in
+function addProp(obj, prop, value) {
+  const newObj = { ...obj }
+  newObj[prop] = value
+  return newObj
+}
+
+const addPropResult = addProp(team, 'accomplishments', [
+  'slam dunk champion',
+  '18 time all-star',
+  '2 time finals MVP',
+  '5 time NBA Champion',
+  '3 scoring title',
+  '2008 NBA MVP',
+])
+console.log(addPropResult)
+console.log()
