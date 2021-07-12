38b85b5417f8bf096c01f48b4ceac7014586ae27
Dom Hallan <dom@polyglotdev.com>
2021-07-12 17:09:34 -0500

    🎆  explore DOM by getElementById and getElementByTagName

--- section-13/app.js
+++ section-13/app.js
@@ -1,1 +1,4 @@
 console.log('Section 13')
+const title = (document.getElementById('title').innerHTML = 'Section 13')
+const getTitle = document.getElementById('title')
+const inputs = document.getElementsByTagName('input')
