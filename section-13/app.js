cfb084fcb12351b3cfd59adede37ff0a4f8078a5
Dom Hallan <dom@polyglotdev.com>
2021-07-12 17:12:01 -0500

    🎆  explore DOM by getElementByClassName

--- section-13/app.js
+++ section-13/app.js
@@ -2,3 +2,6 @@ console.log('Section 13')
 const title = (document.getElementById('title').innerHTML = 'Section 13')
 const getTitle = document.getElementById('title')
 const inputs = document.getElementsByTagName('input')
+
+document.getElementById('title').textContent =
+  'Section 13 - getElementsByClassName'
