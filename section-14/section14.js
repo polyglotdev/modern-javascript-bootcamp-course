2d90e2cd8bb879df70e19c3de2fdb805e83309ab
Dom Hallan <dom@polyglotdev.com>
2021-07-12 18:08:52 -0500

    🎆  use innerHTML to change html element changes thru parsed string

--- section-14/section14.js
+++ section-14/section14.js
@@ -17,3 +17,7 @@ changeH1.innerText = 'Kobe Bryant'
 
 const changefirstP = document.querySelector('p')
 changefirstP.innerText = `Born in Philadelphia and partly raised in Italy, Bryant was recognized as the top American high-school basketball player while at Lower Merion. The son of former NBA player Joe Bryant, he declared for the 1996 NBA draft and was selected by the Charlotte Hornets with the 13th overall pick; he was then traded to the Lakers. As a rookie, Bryant earned a reputation as a high-flyer by winning the 1997 Slam Dunk Contest, and was named an All-Star by his second season. Despite a feud with teammate Shaquille O'Neal, the pair led the Lakers to three consecutive NBA championships from 2000 to 2002.`
+
+// innerHTML - change the innerHTML of the element
+const form = document.querySelector('form')
+// form.innerHTML = `<input type="text" name="firstname" placeholder="First Name">`
