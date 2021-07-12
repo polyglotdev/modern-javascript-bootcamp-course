4d57469524cc9ddacaff6c4f28b3d47b62babfcd
Dom Hallan <dom@polyglotdev.com>
2021-07-12 17:56:49 -0500

    🎆  start section 14

---
+++ section-14/section14.js
@@ -0,0 +1,13 @@
+console.log('Section 14')
+
+// get the h1 element
+const h1 = document.querySelector('h1')
+console.log(h1.innerText)
+
+// get the innerText of ul
+const ul = document.querySelector('ul')
+console.log(ul.innerText)
+
+// get all text in the document
+const text = document.body.innerText
+console.log(text)
