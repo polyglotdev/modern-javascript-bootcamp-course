c2826149258990d77dd88a28ffc4966fe1f21c75
Dom Hallan <dom@polyglotdev.com>
2021-07-12 17:19:17 -0500

    🎆  complete section 13

--- section-13/app.js
+++ section-13/app.js
@@ -3,5 +3,9 @@ const title = (document.getElementById('title').innerHTML = 'Section 13')
 const getTitle = document.getElementById('title')
 const inputs = document.getElementsByTagName('input')
 
-document.getElementById('title').textContent =
-  'Section 13 - getElementsByClassName'
+document.getElementById(
+  'title',
+).textContent = `Section 13 - getElementsByClassName`
+
+const geth1 = document.querySelector('h1')
+console.log(geth1)
